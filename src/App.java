1bc670cab3b8856a8f540ea42c36a82fe9d82ce0
Siraj Mohammed Saleem <sirajms5@yahoo.com>
2023-04-24 11:24:10 -0400

    converting audio to text

--- src/App.java
+++ src/App.java
@@ -1,5 +1,58 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse.BodyHandlers;
+
+import com.google.gson.*;
+
+/*
+* This is the class with method main, it will ask the user
+* for an audio link and send it to assemblyai to convert it into text and output the text
+*/
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        ApiKey apiKey = new ApiKey();
+        final String KEY = apiKey.getKey(); // this variable will have the API key, make sure to use your own key.
+
+        Transcript transcript = new Transcript();
+        transcript.setAudio_url("https://sirajsaleem.com/web/test.mp3");
+        Gson gson = new Gson();
+        String jsonRequest = gson.toJson(transcript); // will convert the provided link into json format
+        System.out.println(jsonRequest);
+
+        // building a POST request
+        HttpRequest postRequest = HttpRequest.newBuilder()
+        .uri(new URI("https://api.assemblyai.com/v2/transcript"))
+        .header("Authorization", KEY)
+        .POST(BodyPublishers.ofString(jsonRequest))
+        .build();
+
+        // sending and restoring POST request
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpResponse<String> postResponse = httpClient.send(postRequest, BodyHandlers.ofString());
+        System.out.println(postResponse.body());
+        
+        // converting the response from json to object
+        transcript = gson.fromJson(postResponse.body(), Transcript.class);
+
+        // building a GET request
+        HttpRequest getRequest = HttpRequest.newBuilder()
+        .uri(new URI("https://api.assemblyai.com/v2/transcript/" + transcript.getId()))
+        .header("Authorization", KEY)
+        .build();
+
+        do {
+        // sending and restoring GET request
+        HttpResponse<String> getResponse = httpClient.send(getRequest, BodyHandlers.ofString());
+        transcript = gson.fromJson(getResponse.body(), Transcript.class);
+        System.out.println(transcript.getStatus());
+        Thread.sleep(1000);
+        } while (!(transcript.getStatus().equals("completed") || transcript.getStatus().equals("error")));
+
+        System.out.println("Transcription completed!");
+        System.out.println(transcript.getText()); // output the text that was converted from the audio
     }
 }
