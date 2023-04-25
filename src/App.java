4c473f6b47778fd355607a260366361390c23e1d
Siraj Mohammed Saleem <sirajms5@yahoo.com>
2023-04-25 10:50:05 -0400

    created loop for multiple entries

--- src/App.java
+++ src/App.java
@@ -7,40 +7,44 @@ import com.google.gson.*;
 public class App {
     public static void main(String[] args) throws Exception {
 
+        String option;
         ApiKey apiKey = new ApiKey();
         final String KEY = apiKey.getKey(); // this variable will have the API key, make sure to use your own key.
-
         User user = new User();
-        String userInput = user.inputString("Enter URL for the audio to be converted: ");
-
-        Transcript transcript = new Transcript();
-        transcript.setAudio_url(userInput);
-        
-        Gson gson = new Gson();
-        String jsonRequest = gson.toJson(transcript); // will convert the provided link into json format
-        System.out.println(jsonRequest);
-
-        AssemblyAiConnector connector = new AssemblyAiConnector();
-        connector.createPostRequest(KEY, jsonRequest); // building a POST request
-        transcript = connector.postResponse(transcript, gson); // sending and restoring POST request
-        connector.createGetRequest(KEY, transcript); // building a GET request
 
         do {
-            transcript = connector.getResponse(transcript, gson); // sending and restoring GET request
-            System.out.println(transcript.getStatus());
-            Thread.sleep(1000);
-        } while (transcript.getStatus() != null
-                && !(transcript.getStatus().equals("completed") || transcript.getStatus().equals("error")));
+            String userInput = user.inputString("Enter URL for the audio to be converted: ");
 
-        if (transcript.getStatus() != null
-                && transcript.getStatus().equals("completed")) {
-            System.out.println("Transcription completed!");
-            String text = transcript.getText();
-            text = text.replaceAll("([?.])(\\s)", "$1\n"); // rearrange the converted audio into seperated lines
-            System.out.println(text); // output the text that was converted from the audio
-        } else {
-            System.out.println(
-                    "*** Make sure that the link is for an audio/video file and not a website such as YouTube. ***");
-        }
+            Transcript transcript = new Transcript();
+            transcript.setAudio_url(userInput);
+
+            Gson gson = new Gson();
+            String jsonRequest = gson.toJson(transcript); // will convert the provided link into json format
+            System.out.println(jsonRequest);
+
+            AssemblyAiConnector connector = new AssemblyAiConnector();
+            connector.createPostRequest(KEY, jsonRequest); // building a POST request
+            transcript = connector.postResponse(transcript, gson); // sending and restoring POST request
+            connector.createGetRequest(KEY, transcript); // building a GET request
+
+            do {
+                transcript = connector.getResponse(transcript, gson); // sending and restoring GET request
+                System.out.println(transcript.getStatus());
+                Thread.sleep(1000);
+            } while (transcript.getStatus() != null
+                    && !(transcript.getStatus().equals("completed") || transcript.getStatus().equals("error")));
+
+            if (transcript.getStatus() != null
+                    && transcript.getStatus().equals("completed")) {
+                System.out.println("Transcription completed!");
+                String text = transcript.getText();
+                text = text.replaceAll("([?.])(\\s)", "$1\n"); // rearrange the converted audio into seperated lines
+                System.out.println(text); // output the text that was converted from the audio
+            } else {
+                System.out.println(
+                        "*** Make sure that the link is for an audio/video file and not a website such as YouTube. ***");
+            }
+            option = user.inputString("Would you like to enter another url? (Y/N): ");
+        } while (option.equalsIgnoreCase("Y"));
     }
 }
--- src/User.java
+++ src/User.java
@@ -4,6 +4,7 @@ public class User {
     
     private Scanner keyboard = new Scanner(System.in);
     
+    // inputString methood will request user to input a string and it will return it
     public String inputString(String message){
         System.out.print(message);
         String userInput = keyboard.nextLine();
